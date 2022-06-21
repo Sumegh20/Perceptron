c92f71ecf7799c246e4af62a6cd2fa0d6df35b75
Sumegh20 <sumegh20@gmail.com>
2022-06-21 14:51:23 +0530

    Add loging in or.py and and.py

--- and.py
+++ and.py
@@ -1,7 +1,19 @@
+import os
 import pandas as pd
 
 from utils.all_utils import X_y_split, save_plot
 from utils.model import Perceptron
+import logging
+
+gate = 'OR Gate'
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join("logs", "logfile.log"),
+    level=logging.INFO,
+    format='[%(asctime)s: %(levelname)s: %(module)s]: %(message)s',
+    filemode='a'
+    )
 
 def main(df, modelName, plotName, eta, epochs):
     X,y = X_y_split(df)
@@ -25,4 +37,10 @@ if __name__ == "__main__":
     ETA = 0.1
     EPOCHS = 10
 
-    main(df=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
+    try:
+        logging.info(f"_____START TRANING_____  {gate}")
+        main(df=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
+        logging.info(f'_____DONE TRANING_____  {gate}\n\n')
+    except Exception as e:
+        logging.exception(e)
+        raise e
